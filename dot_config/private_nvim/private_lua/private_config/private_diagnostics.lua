a4b98d647e0e910859a2cbea3e8b1d5d112563d2
Jason Kenney <1273829+therealjasonkenney@users.noreply.github.com>
2025-01-01 23:05:24 -0500

    Move diagnostics to config folder

---
+++ dot_config/private_nvim/private_lua/private_config/private_diagnostics.lua
@@ -0,0 +1,31 @@
+local function float_prefix(diagnostic, i, total)
+	local prefixes = {}
+
+	prefixes[vim.diagnostic.severity.ERROR] = "E"
+	prefixes[vim.diagnostic.severity.HINT] = "H"
+	prefixes[vim.diagnostic.severity.INFO] = "I"
+	prefixes[vim.diagnostic.severity.WARN] = "W"
+
+	local format = "[%1s (%02d/%02d)]: "
+	local sign = prefixes[diagnostic.severity]
+
+	return string.format(format, sign, i, total)
+end
+
+return {
+	config = function()
+		vim.diagnostic.config({
+			float = {
+				border = "rounded",
+				source = true,
+				header = "Diagnostics\n--------",
+				prefix = float_prefix,
+			},
+			severity_sort = false,
+			signs = true,
+			underline = true,
+			update_in_insert = false,
+			virtual_text = false,
+		})
+	end,
+}
