f8e302f7e5bc6b2ca92362438a8dc769e95d95b6
Lain <1273829+therealjasonkenney@users.noreply.github.com>
2025-03-30 16:05:52 -0400

    Changed editor config so we use cli neovim when in command line.

--- dot_config/fish/conf.d/nvim.fish
+++ dot_config/fish/conf.d/nvim.fish
@@ -1,12 +1,6 @@
 # Only load if its Interactive
-status is-interactive; type -q nvim; and begin
-
-  # Use neovim as the editor.
-  set -gx EDITOR nvim
-end
-
 status is-interactive; type -q neovide; and begin
 
-  # Use neovide as fullscreen editor.
-  set -gx VISUAL neovide
+  # Use nvim as fullscreen editor.
+  set -gx VISUAL nvim
 end
