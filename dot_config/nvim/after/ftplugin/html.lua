fb9636a30fb39bdaf670d68b0a0c7a9591337ef4
Jason Kenney <1273829+therealjasonkenney@users.noreply.github.com>
2026-07-06 16:21:05 -0400

    Refactored and fixed neovim for js and rust development.

--- dot_config/nvim/after/ftplugin/html.lua
+++
@@ -1,9 +0,0 @@
--- For HTML we only need formatting and syntax highlighting.
-
-local util = require("util")
-
-local bufnr = vim.api.nvim_get_current_buf()
-
-util.ensure_installed("prettierd", function()
-  util.enable_formatting(bufnr, false)
-end)
