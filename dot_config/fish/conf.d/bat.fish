29c8bd8b217836cc6b5a1bad9c71c3d4163328e6
Jason Kenney <1273829+therealjasonkenney@users.noreply.github.com>
2026-09-24 15:57:10 -0400

    Fix bat

--- dot_config/fish/conf.d/bat.fish
+++ dot_config/fish/conf.d/bat.fish
@@ -1,6 +1,5 @@
-# Only load if its Interactive
-status is-interactive; type -q bat; and begin
+status is-interactive; and begin
 
-  # Use bat for manpaging
-  set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+    # Use bat for manpaging
+    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 end
